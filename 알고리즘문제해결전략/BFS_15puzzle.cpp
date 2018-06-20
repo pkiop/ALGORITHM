73ca8f355eb795e5417b033ea4ce6eb72dba029d
pkiop <vivezdv@gmail.com>
2018-06-20 09:52:15 +0900

    15puzzle 추가
    양방향 탐색, 점점 깊어지는 탐색 추가

--- 알고리즘문제해결전략/BFS_15puzzle.cpp
+++ 알고리즘문제해결전략/BFS_15puzzle.cpp
@@ -1,7 +1,7 @@
-/* 
-	메모리 사용하는게 선형으로 증가. 
-	시간 매우 오래걸리고 메모리도 많이 사용 
-
+/*
+	양 방향 탐색.
+	완료조건이 명확한 경우 반대 방향에서도 출발해서 중간에 만나게 해서 더 
+	메모리와 시간을 단축 시킬 수 있다. 
 */
 
 #include <iostream>
@@ -87,7 +87,41 @@ public:
 	}
 };
 
+int sgn(int x) { // 부호반환 함수
+	if (!x) return 0; // 0이면 0 return
+	return x > 0 ? 1 : -1;
+}
+
+int incr(int x) { if (x < 0) return x - 1; return x + 1; } // 역방향은 더 작게 순방향은 더 크게
+
 typedef map<State, int> StateMap;
+
+int bidirectional(State start, State finish) {
+	StateMap c;
+	queue<State> q;
+	if (start == finish) return 0;
+	q.push(start);
+	c[start] = 1;
+	q.push(finish);
+	c[finish] = -1;
+
+	while (!q.empty()) {
+		State here = q.front();
+		q.pop();
+
+		vector<State> adjacent = here.getAdjacent();
+		for (int i = 0; i < adjacent.size(); ++i) {
+			map<State, int>::iterator it = c.find(adjacent[i]);
+			if (it == c.end()) { // end는 null 과 같은 의미로 이걸 쓴 듯. 비어있다는 뜻 같은데 
+				c[adjacent[i]] = incr(c[here]);
+				q.push(adjacent[i]);
+			}
+			else if (sgn(it->second) != sgn(c[here])) // 가운데서 만난 경우. 부호가 다른 것 끼리 만나면 그렇다.
+				return abs(it->second) + abs(c[here]) - 1;
+		}
+	}
+	return -1;
+}
 
 int bfs(State start, State finish) {
 	if (start == finish) {
@@ -127,9 +161,37 @@ int bfs(State start, State finish) {
 	return -1;
 }
 
+// 점점 깊어지는 탐색
+
+int best;
+
+void dfs(State here, const State& finish, int steps) {
+	if (steps >= best) return;
+
+	if (here == finish) {
+		best = steps; return;
+	}
+
+	vector<State> adjacent = here.getAdjacent();
+	for (int i = 0; i < adjacent.size(); ++i) {
+		dfs(adjacent[i], finish, steps + 1);
+	}
+}
+
+int ids(State start, State finish, int growthStep) {
+	for (int limit = 4; ; limit += growthStep) {
+		best = limit + 1;
+		dfs(start, finish, 0);
+		if (best <= limit) return best;
+	}
+	return -1;
+}
+
+// 점점 깊어지는 탐색 끝
+
 int main(void) {
 	vector<int> arr;
-	arr.push_back(1);
+	arr.push_back(0);
 	arr.push_back(2);
 	arr.push_back(3);
 	arr.push_back(4);
@@ -137,13 +199,13 @@ int main(void) {
 	arr.push_back(6);
 	arr.push_back(7);
 	arr.push_back(8);
-	arr.push_back(0);
+	arr.push_back(9);
 	arr.push_back(10);
 	arr.push_back(11);
 	arr.push_back(12);
 	arr.push_back(13);
 	arr.push_back(14);
-	arr.push_back(9);
+	arr.push_back(1);
 	arr.push_back(15);
 
 	vector<int> arrf;
@@ -167,7 +229,7 @@ int main(void) {
 	State My(arr);
 	State finish(arrf);
 
-	cout << bfs(My, finish) << endl;
+	cout << bidirectional(My, finish) << endl;
 
 	return 0;
 
