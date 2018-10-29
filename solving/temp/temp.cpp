5fda38413156eadda7bccab32d37118f1c17fbcf
pkiop <vivezdv@gmail.com>
2018-10-29 13:32:53 +0900

    .

---
+++ solving/temp/temp.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    int a = 2;
+    int b = 3;
+    int c = a + b;
+
+    cout << a << " + " << b << " = " << a + b << '\n';
+
+    return 0;
+}
