5f5885b16adb8b867e688c20ee361d6dca75e753
Park Sang Shin <34783156+pkiop@users.noreply.github.com>
2020-01-19 23:04:48 +0900

    Create b5339.cpp

---
+++ BOJ/문제들/b5339.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+	
+
+	cout << "     /~\\" << endl;
+    cout << "    ( oo|" << endl;
+    cout << "    _\\=/_" << endl;
+   cout << "   /  _  \\" << endl;
+  cout << "  //|/.\\|\\\\" << endl;
+  cout << " ||  \\ /  ||" << endl;
+  cout << "============" << endl;
+  cout << "|          |" << endl;
+  cout << "|          |" << endl;
+    cout << "|          |" << endl;
+
+}
