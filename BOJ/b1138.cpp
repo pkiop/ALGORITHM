59a36b4c58d3180be44374d02c3017538a39ea28
Park Sang Shin <34783156+pkiop@users.noreply.github.com>
2020-03-03 20:59:31 +0900

    Create b1138.cpp

---
+++ BOJ/b1138.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+vector<int> arr;
+
+bool visited[1001];
+vector<int> ans;
+bool flag = false;
+int n;
+void dfs(int depth) {
+	if(ans.size() == n) {
+		for(int i=0;i<ans.size();++i) {
+			cout << ans[i]+1 << ' ';	
+
+		}
+		flag = true;
+		return ;
+	}
+	for(int i=0;i<n;++i) { 
+		if(visited[i] == false) {
+			int big_cnt = 0;
+			for(int j=0;j<depth;++j) {
+				if(ans[j] > i) {
+					big_cnt++;
+				}
+			}
+			if(big_cnt == arr[i]) {
+				ans.push_back(i);
+				visited[i] = true;
+				dfs(depth+1);
+				if(flag) {
+					return;
+				}
+				visited[i] = false;
+				ans.pop_back();
+			}
+		}
+	}
+}
+int main(void) {
+	cin >> n;
+	int temp;
+	for(int i=0;i<n;++i) {
+		cin >> temp;
+		arr.push_back(temp);
+	}
+
+	dfs(0);
+	return 0;
+
+}
